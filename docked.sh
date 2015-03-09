80ce48c49f23d75ba87b476d94c6e2c1fde8937f
Will DeBerry <willdeberry@gmail.com>
2015-03-09 12:29:41 -0400

    docked: change hdmi port number

--- docked.sh
+++ docked.sh
@@ -3,7 +3,7 @@
 while [ -n "${1}" ]; do
 	case "${1}" in
 		-w | --work)
-			xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI2 --off --output HDMI3 --mode 1920x1080 --pos 1600x0 --rotate normal --output HDMI1 --off --output LVDS1 --mode 1600x900 --pos 0x0 --rotate normal --output VGA1 --off
+			xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI2 --mode 1920x1080 --pos 1600x0 --rotate normal --output HDMI3 --off --output HDMI1 --off --output LVDS1 --mode 1600x900 --pos 0x0 --rotate normal --output VGA1 --off
 			nitrogen --restore
 			exit
 			;;
