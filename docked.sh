6c8f8b190bbe79ea1a1912424b5fcbff58cef997
Will DeBerry <willdeberry@gmail.com>
2016-05-25 15:23:46 -0400

    Fix mouse on docking

--- docked.sh
+++ docked.sh
@@ -5,11 +5,13 @@ while [ -n "${1}" ]; do
 		-w | --work)
 			xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI2 --mode 1920x1080 --pos 1600x0 --rotate normal --output HDMI3 --off --output HDMI1 --off --output LVDS1 --mode 1600x900 --pos 0x0 --rotate normal --output VGA1 --off
 			nitrogen --restore
+			xset m 10/10 4
 			exit
 			;;
 		*)
-			xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output VGA1 --mode 1680x1050 --pos 1600x0 --rotate normal --output HDMI1 --off --output LVDS1 --mode 1600x900 --pos 0x0 --rotate normal --output HDMI2 --off
+			xrandr --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output VGA1 --mode 1920x1080 --pos 1600x0 --rotate normal --output HDMI1 --off --output LVDS1 --mode 1600x900 --pos 0x0 --rotate normal --output HDMI2 --off
 			nitrogen --restore
+			xset m 10/10 4
 			exit
 			;;
 	esac
